8ea8457911c20641a9777c9954654f3c82e7b555
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2024-07-06 12:06:01 +0000

    app.js Event module 1 updated

---
+++ Event module 1/app.js
@@ -0,0 +1,62 @@
+
+// const events = require("events");
+
+// const {EventEmitter} = events;
+
+// const eventEmitter = new EventEmitter();
+
+// //register an event
+// eventEmitter.on("event-1", function(){
+//     console.log("Hi there!");
+// });
+
+// //emit or raise an event
+// eventEmitter.emit("event-1");
+
+
+// //register an event
+// eventEmitter.on("event-1", function(param1){
+//     console.log("Hi there!");
+//     console.log(param1);
+// });
+
+// //emit or raise an event
+// eventEmitter.emit("event-1", "by there!");
+
+
+
+// //register an event
+// eventEmitter.on("event-1", function(param1, value){
+//     console.log("Hi there!");
+//     console.log(param1);
+//     console.log(value);
+// });
+
+// //emit or raise an event
+// eventEmitter.emit("event-1", "by there!", 10);
+
+
+
+
+// //register an event
+// eventEmitter.on("event-1", function(param1, value, name){
+//     console.log("Hi there!");
+//     console.log(param1);
+//     console.log(value);
+//     console.log(name);
+// });
+
+// //emit or raise an event
+// eventEmitter.emit("event-1", "by there!", 10, "Vaibhav");
+
+
+
+// //register an event
+// eventEmitter.on("event-1", function(obj){
+//     console.log("Hi there!");
+//     console.log(obj);
+// })
+
+// //emit or raise an event
+// eventEmitter.emit("event-1", {msg: "bye there!", age: "20", name: "vaibhav"});
+
