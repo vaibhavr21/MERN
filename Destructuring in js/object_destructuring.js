deb1f96477eef955a792d507c48a2c47b7cdd211
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2024-10-13 14:43:56 +0000

    object more examples

--- Destructuring in js/object_destructuring.js
+++ Destructuring in js/object_destructuring.js
@@ -10,3 +10,35 @@ const {name, language,
 
 console.log(name);
 console.log(language);
+
+
+
+// object destructuring
+const profile = {
+    name : 'code',
+    language : 'javascript'
+}
+
+const {name, language, java
+} = profile;
+
+console.log(name);
+console.log(language);
+console.log(java)   // undefined
+
+
+
+
+
+// object destructuring
+const profile = {
+    name : 'code',
+    language : 'javascript'
+}
+
+const {name, language,
+} = profile;
+
+console.log(name);
+console.log(language);
+// console.log(java)   // uncaught referemceError java is not defined
