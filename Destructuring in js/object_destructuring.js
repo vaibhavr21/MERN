bdabc0a0c791499b4520c55bd86941b23f503b5d
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2024-10-13 14:42:30 +0000

    object destructuring

---
+++ Destructuring in js/object_destructuring.js
@@ -0,0 +1,12 @@
+
+// object destructuring
+const profile = {
+    name : 'code',
+    language : 'javascript'
+}
+
+const {name, language,
+} = profile;
+
+console.log(name);
+console.log(language);
