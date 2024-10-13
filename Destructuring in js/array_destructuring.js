b760f4ed1f36ad6ccb3051156c9b8b16bb2676aa
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2024-10-13 14:41:04 +0000

    array more examples

--- Destructuring in js/array_destructuring.js
+++ Destructuring in js/array_destructuring.js
@@ -5,3 +5,30 @@ const [firstColor, secondColor] = colors;
 
 console.log(firstColor)  //blue
 console.log(secondColor) //red
+
+
+
+
+// array destructuring
+const colors = ['blue','red', 'yellow'];
+
+const [firstColor, secondColor, thirdColor,] = colors;
+
+console.log(firstColor)
+console.log(secondColor)
+console.log(thirdColor)
+console.log(forthColor)     //ReferenceError forthColor is not defined
+
+
+
+
+
+// array destructuring
+const colors = ['blue','red', 'yellow'];
+
+const [firstColor, secondColor, thirdColor, forthColor] = colors;
+
+console.log(firstColor)
+console.log(secondColor)
+console.log(thirdColor)
+// console.log(forthColor)     //undefined
