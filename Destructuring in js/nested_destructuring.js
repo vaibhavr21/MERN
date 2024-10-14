8aa136a52bbf0ab658dedd6330a9283400b8a344
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2024-10-14 18:00:29 +0000

    another example of nested destructuring

--- Destructuring in js/nested_destructuring.js
+++ Destructuring in js/nested_destructuring.js
@@ -15,3 +15,22 @@ const {
 
 console.log(nem)
 console.log(math)
+
+
+
+const student = {
+    nem : 'code',
+    scores: {
+        math : 80,
+        science : 86
+    }
+};
+
+const {
+    nem,
+    scores:{math,science}
+} = student;
+
+console.log(nem)
+console.log(math)
+console.log(science)
